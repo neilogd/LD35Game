1f410cde0ef4da8497903db58b0cfcadc97ec660
Neil Richardson <neilo@neilo.gd>
2016-04-16 12:15:57 +1000

    Added math

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,81 @@
+extern crate sdl2;
+
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::rect::Point;
+
+pub mod math;
+
+use math::Vec2d;
+
+const WIDTH: i32 = 1024;
+const HEIGHT: i32 = 768;
+
+
+/////////////////////////////////////////////////////////////////////
+// Utility
+impl Vec2d
+{
+	fn get_point(&self) -> Point
+	{
+		Point::new(self.x as i32, self.y as i32)
+	}
+}
+
+
+/////////////////////////////////////////////////////////////////////
+// main
 fn main()
 {
-	println!("This is a game for Ludum Dare");
+	let ctx = sdl2::init().unwrap();
+	let video_ctx = ctx.video().unwrap();
+
+	let window = match video_ctx.window("LD35Game", WIDTH as u32, HEIGHT as u32).position_centered().opengl().build()
+	{
+		Ok(window) => window,
+		Err(err) => panic!("Failed to create window: {}", err)
+	};
+
+
+	let mut renderer = window.renderer().build().unwrap();
+
+	renderer.set_draw_color(Color::RGB(0, 0, 0));
+	renderer.clear();
+	renderer.present();
+
+	let mut event_pump = ctx.event_pump().unwrap();
+
+	let mut position = Vec2d::new(WIDTH as f32, HEIGHT as f32) * 0.5;
+
+	let mut timer = 0.0f32;
+	let tick = 1.0f32 / 60.0f32;
+
+	'running: loop
+	{
+		for event in event_pump.poll_iter()
+		{
+			match event
+			{
+				Event::Quit {..} => break 'running,
+				_ => {}
+			}
+		}
+
+		// Clear screen.
+		renderer.set_draw_color(Color::RGB(0, 0, 0));
+		renderer.clear();
+
+		let offset = Vec2d::new(timer.cos(), timer.sin());
+		let point_a = position - (offset * 128.0);
+		let point_b = position + (offset * 128.0);
+
+		renderer.set_draw_color(Color::RGB(0, 192, 0));
+		renderer.draw_line(point_a.get_point(), point_b.get_point());
+
+		renderer.present();
+
+		timer = timer + tick;
+	}
 }
+
---
+++ src/math.rs
@@ -0,0 +1,126 @@
+use std::ops::{ Add, Sub, Mul, Div };
+
+/////////////////////////////////////////////////////////////////////
+// Vec2d
+#[derive(Copy, Clone)]
+pub struct Vec2d
+{
+	pub x: f32,
+	pub y: f32,
+}
+
+impl Vec2d
+{
+	pub fn new(in_x: f32, in_y: f32) -> Vec2d
+	{
+		Vec2d
+		{
+			x: in_x,
+			y: in_y,
+		}
+	}
+}
+
+impl Add<Vec2d> for Vec2d
+{
+	type Output = Vec2d;
+	fn add(self, rhs: Vec2d) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x + rhs.x,
+			y: self.y + rhs.y,
+		}
+	}
+}
+
+impl Add<f32> for Vec2d
+{
+	type Output = Vec2d;
+	fn add(self, rhs: f32) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x + rhs,
+			y: self.y + rhs,
+		}
+	}
+}
+
+impl Sub<Vec2d> for Vec2d
+{
+	type Output = Vec2d;
+	fn sub(self, rhs: Vec2d) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x - rhs.x,
+			y: self.y - rhs.y,
+		}
+	}
+}
+
+impl Sub<f32> for Vec2d
+{
+	type Output = Vec2d;
+	fn sub(self, rhs: f32) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x - rhs,
+			y: self.y - rhs,
+		}
+	}
+}
+
+impl Mul<Vec2d> for Vec2d
+{
+	type Output = Vec2d;
+	fn mul(self, rhs: Vec2d) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x * rhs.x,
+			y: self.y * rhs.y,
+		}
+	}
+}
+
+impl Mul<f32> for Vec2d
+{
+	type Output = Vec2d;
+	fn mul(self, rhs: f32) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x * rhs,
+			y: self.y * rhs,
+		}
+	}
+}
+
+impl Div<Vec2d> for Vec2d
+{
+	type Output = Vec2d;
+	fn div(self, rhs: Vec2d) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x / rhs.x,
+			y: self.y / rhs.y,
+		}
+	}
+}
+
+impl Div<f32> for Vec2d
+{
+	type Output = Vec2d;
+	fn div(self, rhs: f32) -> Vec2d
+	{
+		Vec2d
+		{
+			x: self.x / rhs,
+			y: self.y / rhs,
+		}
+	}
+}
